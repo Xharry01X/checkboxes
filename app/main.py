19b3d720687c4e79fd732dff0be16a45145913c0
harshit-506 <supriyamohapatra658@gmail.com>
2024-08-20 23:15:51 +0530

    template is added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def index():
-    return "Hello world"
-    
+def index(request: Request):
+    return templates.TemplateResponse(
+        name="home.html",
+        context={"request": request}
+    )
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import uvicorn
-
+from app.main import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        app="app.main:app",
+        app,
         host="localhost",
         port=8000,
         reload=True
